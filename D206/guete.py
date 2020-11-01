60df73e446cf27b24dad3d1895fceaadaecb1242
sonia <sonia.chander@tu-dortmund.de>
2020-11-01 20:57:43 +0100

    ideale Güteziffer jetzt bei

--- D206/guete.py
+++ D206/guete.py
@@ -11,12 +11,12 @@ import numpy as np
 from scipy.optimize import curve_fit
 
 #Aufgabe b
-X, Y, Z, N= [],[], [], []
+X, T1, T2, N= [],[], [], []
 for line in open('data.txt', 'r'):
   values = [float(s) for s in line.split()]
   X.append(values[0])
-  Y.append(values[1]+273.15)
-  Z.append(values[3]+273.15)
+  T1.append(values[1]+273.15)
+  T2.append(values[3]+273.15)
   N.append(values[5])
 
 def line(X, A, B, C):
@@ -28,20 +28,24 @@ def ableitung(X,A,B): #für Aufgabe c
 def realGute(ab, N): #für Aufgabe d
     return ((4*4184 + 750)*ab)/N  #J/KW
 
-popt, pcov = curve_fit(line, X, Y)
+def idealGute(a,b):
+    return a/(a-b)
+
+popt, pcov = curve_fit(line, X, T1)
 
 print("T1")
 print("A =", popt[0])
 print("B =", popt[1])
 print("C =", popt[2])
 
-for i in range(4): #einfach die ersten vier Punkte
+for i in range(1,4): #einfach die ersten vier Punkte
     print("ableitung bei t = ", X[i])
     abl = ableitung(X[i], popt[0], popt[1])
     print(abl)
     print("reale Güteziffer hier ", realGute(abl, N[i]))
+    print("ideale Güteziffer hier ", idealGute(T1[i], T2[i]))
 
-popt, pcov = curve_fit(line, X, Z)
+popt, pcov = curve_fit(line, X, T2)
 
 print("T2")
 print("A =", popt[0])
@@ -49,11 +53,13 @@ print("B =", popt[1])
 print("C =", popt[2])
 
 
-for i in range(4):
+for i in range(1,4):
     print("ableitung bei t = ", X[i])
     abl = ableitung(X[i], popt[0], popt[1])
     print(abl)
     print("reale Güteziffer hier ", realGute(abl, N[i]))
+    print("ideale Güteziffer hier ", idealGute(T1[i], T2[i]))
+
 
 
 
