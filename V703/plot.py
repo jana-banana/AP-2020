45dc2f61e9229cdd4557cda51fb3a2b93db75d46
sonia <sonia.chander@tu-dortmund.de>
2021-01-23 18:25:52 +0100

    Aufgabe a

--- V703/plot.py
+++ V703/plot.py
@@ -1,21 +1,39 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import curve_fit
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+#Aufgabe a Zählrohr-Charakteristik
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+#Diagramm mit Fehlerbalken
+U, N = np.genfromtxt('Kennlinie.txt', unpack=True)
+N_err = np.sqrt(N)
+plt.errorbar(U, N ,yerr=N_err, fmt ='.')
+plt.xlabel('Spannung U [V]')
+plt.ylabel('registrierte Teilchenanzahl N [Imp/60s]')
+plt.plot(U,N, '.',label='Charakteristik')
+plt.tight_layout()
+plt.savefig('Zählrohrcharakteristik.pdf')
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+#Ausgleichsrechnung vom Plateau
+U_Plateau, N_Plateau=[],[]
+for i in range (23):
+    U_Plateau.append(U[i+8])
+    N_Plateau.append(N[i+8])
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+def line(U,a,b):
+    return a*U+b
+
+popt, pcov = curve_fit(line, U_Plateau, N_Plateau)
+
+print("a =", popt[0])
+print("b =", popt[1])
+
+plt.plot(U, line(U, popt[0], popt[1]), 'r-')
+plt.tight_layout()
+plt.savefig('Plateau_Gerade.pdf')
+
+
+print('x1 =', line(0,popt[0],popt[1]))
+print('x2 =', line(100,popt[0],popt[1]))
+
+#Aufgabe b Primär- und Nachentladungsimpulsen
