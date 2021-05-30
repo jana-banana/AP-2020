42bdb77669de037160c4ab45cbe403e5c7fa4aba
sonia <sonia.chander@tu-dortmund.de>
2021-05-30 22:05:36 +0200

    schlimm

--- V303/plot.py
+++ V303/plot.py
@@ -10,8 +10,11 @@ phi, A_ohne, A_mit= np.genfromtxt('data/data_ohne_mit.txt', unpack=True)
 
 phi *= np.pi/180
 
-def U(phi, U0, B):
-    return 2/np.pi* U0 * np.cos(phi+B)
+#def U(phi, U0, B):
+#    return 2/np.pi* U0 * np.cos(phi+B)
+
+def U(phi, U_0, kA):
+    return abs(U_0 * np.cos(phi))
 
 par, covm = curve_fit(
     U,
@@ -61,6 +64,18 @@ plt.plot(r[1:], A_dioden[1:],'.', label='Messwerte')
 #plt.plot(phi , A_dioden , '.', label = 'Messwerte')
 plt.xlabel('r /cm')
 plt.ylabel('Spannung $U$ [V]')
+
+def line_dioden(r, a, b):
+    return a* 1/(r) + b
+
+popt, pcov = curve_fit(line_dioden, r, A_dioden)
+errors = np.sqrt(np.diag(pcov))
+print("a =", popt[0])
+print("a fehler =",errors[0])
+print("b =", popt[1])
+print("b fehler=",errors[1])
+
+plt.plot(r, line_dioden(r, popt[0], popt[1]),'r-', label='Ausgleichgerade')
 
 plt.grid()
 plt.tight_layout()
