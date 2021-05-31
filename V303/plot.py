25742a0b24087f1308aa5a0156c1f1b12a02bc23
Jana Schlücking <jana.schluecking@tu-dortmund.de>
2021-05-31 14:26:42 +0200

    plot.py und data changend und es läuft durch soweit

--- V303/plot.py
+++ V303/plot.py
@@ -2,6 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import uncertainties.unumpy as unp
 from scipy.optimize import curve_fit
+import os
+
+if os.path.exists("bilder") == False:
+    os.mkdir("bilder")
 
 #AUFGABE 1 ohne Noise generator
 print('AUFGABE 1')
@@ -10,21 +14,25 @@ phi, A_ohne, A_mit= np.genfromtxt('data/data_ohne_mit.txt', unpack=True)
 
 phi *= np.pi/180
 
-#def U(phi, U0, B):
-#    return 2/np.pi* U0 * np.cos(phi+B)
+def U(phi, U0, B, c):
+   return U0 * np.cos(phi+B) +c 
 
-def U(phi, U_0, kA):
-    return abs(U_0 * np.cos(phi))
+# def U(phi, U_0, kA):
+    # return abs(U_0 * np.cos(phi))
 
 par, covm = curve_fit(
     U,
     phi,
     A_ohne,
-    sigma=None,
-    absolute_sigma=True,
-    p0=[10, 1]
+    p0=[10, 0, 0]
     )
+# sigma=None,
+# absolute_sigma=True,
 err = np.sqrt(np.diag(covm))
+
+print('U0=', par[0],'+-', err[0])
+print('B =', par[1],'+-', err[1])
+print('c =', par[2],'+-', err[2])
 
 plt.plot(phi , A_ohne , '.', label = 'Messwerte ohne')
 x_plot = np.linspace(0, 2*np.pi, 1000)
@@ -36,20 +44,34 @@ plt.plot(x_plot,U(x_plot, *par) , 'r-', label="Nicht-Lineare Regression")
 plt.grid()
 plt.tight_layout()
 plt.legend()
-#plt.savefig('content/abbildungen/plot1.pdf')
+plt.savefig('bilder/plot1.pdf')
 
 plt.clf()
 
 #AUFGABE 1 mit Noise generator
 
+par, covm = curve_fit(
+    U,
+    phi,
+    A_mit,
+    p0=[10, 0, 0]
+    )
+# sigma=None,
+# absolute_sigma=True,
+err = np.sqrt(np.diag(covm))
+print('U0=', par[0],'+-', err[0])
+print('B =', par[1],'+-', err[1])
+print('c =', par[2],'+-', err[2])
+
 plt.plot(phi , A_mit , '.', label = 'Messwerte mit')
+plt.plot(x_plot,U(x_plot, *par) , 'r-', label="Nicht-Lineare Regression")
 plt.xlabel('$\phi$')
 plt.ylabel('Spannung $U$ [V]')
 
 plt.grid()
 plt.tight_layout()
 plt.legend()
-#plt.savefig('content/abbildungen/plot2.pdf')
+plt.savefig('bilder/plot2.pdf')
 
 plt.clf()
 
